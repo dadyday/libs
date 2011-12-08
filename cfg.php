890e53a4558012ca3547f435d4b0009cb79267d6
dadyday <github.com@holgerheyne.de>
2011-12-08 14:39:30 +0100

    erste Version

---
+++ cfg.php
@@ -0,0 +1,9 @@
+<?php
+
+	@define('URL_ROOT', 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/');
+	@define('PATH_LIBS', dirname(__FILE__).'/');
+	
+	//require_once(PATH_LIBS . 'codegen/cfg.php');
+	//require_once(PATH_LIBS . 'common/cfg.php');
+	//require_once(PATH_LIBS . 'bank/cfg.php');
+?>
